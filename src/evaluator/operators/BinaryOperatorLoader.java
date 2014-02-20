7a9c1da1f6dcdda40dabf829c8b1a19652939f41
Rubén Díaz Martínez <rubendm92@gmail.com>
2014-02-20 17:44:59 +0000

    Some refactor in BinaryOperatorLoader

--- src/evaluator/operators/BinaryOperatorLoader.java
+++ src/evaluator/operators/BinaryOperatorLoader.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
@@ -12,10 +13,8 @@ import java.util.logging.Logger;
 public class BinaryOperatorLoader {
 
     public ArrayList<Class> loadClasses() {
-        ArrayList<Class> operators = new ArrayList<>();
         try {
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            assert classLoader != null;
             String path = "evaluator.operators.".replace('.', '/');
             Enumeration<URL> resources = classLoader.getResources(path);
             List<File> dirs = new ArrayList<>();
@@ -27,33 +26,39 @@ public class BinaryOperatorLoader {
             for (File directory : dirs) {
                 classes.addAll(findClasses(directory, "evaluator.operators"));
             }
-            for (Class i : classes) {
-                Class[] interfaces = i.getInterfaces();
-                if (interfaces.length == 0) {
-                    continue;
-                }
-                if (interfaces[0] == BinaryOperator.class) {
-                    operators.add(i);
-                }
-            }
+            return getSubclassesImplementingInterface(classes, BinaryOperator.class);
         } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(BinaryOperatorLoader.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return new ArrayList<>();
+    }
+
+    private ArrayList<Class> getSubclassesImplementingInterface(ArrayList<Class> classes, Class requestedInterface) {
+        ArrayList<Class> operators = new ArrayList<>();
+        for (Class i : classes)
+            if (classImplementsInterface(i, requestedInterface))
+                operators.add(i);
         return operators;
+    }
+    
+    private boolean classImplementsInterface(Class classObject, Class requestedInterface) {
+        List<Class> interfaces = Arrays.asList(classObject.getInterfaces());
+        return interfaces.contains(requestedInterface);
     }
 
     private List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<>();
-        if (!directory.exists()) {
-            return classes;
-        }
+        if (!directory.exists()) return new ArrayList<>();
         File[] files = directory.listFiles();
+        return getClasses(files, packageName);
+    }
+
+    private List<Class> getClasses(File[] files, String packageName) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<>();
         for (File file : files) {
-            if (file.isDirectory()) {
+            if (file.isDirectory())
                 classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-            }
+            else if (file.getName().endsWith(".class"))
+                classes.add(Class.forName(packageName + '.' + file.getName().replace(".class", "")));
         }
         return classes;
     }
