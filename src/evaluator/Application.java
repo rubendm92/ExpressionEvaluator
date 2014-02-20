b1848700180d0bb0000d342d3371472518f5d75d
Rubén Díaz Martínez <rubendm92@gmail.com>
2014-02-20 09:13:31 +0000

    EvaluatorTests file created

---
+++ src/evaluator/Application.java
@@ -0,0 +1,7 @@
+package evaluator;
+
+public class Application {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ test/evaluator/EvaluatorTests.java
@@ -0,0 +1,7 @@
+package evaluator;
+
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class EvaluatorTests {
+}
