d960b51e4231f75e2b6bd31be13ee1542a785785
rubendm <rubendm@10.230.175.167>
2014-03-17 18:59:39 +0000

    Lexer analyzes simple expressions

--- test/lexer/Lexer.java
+++ test/lexer/Lexer.java
@@ -1,13 +1,44 @@
 package lexer;
 
+import java.util.ArrayList;
 import parser.token.Constant;
 import parser.token.Operator;
 import parser.token.Token;
 
 public class Lexer {
 
-    public Token[] analyze(String expression) {
-        return new Token[]{new Constant(1), Operator.ADD, new Constant(2)};
+    public ArrayList<Token> analyze(String expression) {
+        ArrayList<Token> tokens = new ArrayList<>();
+        String number = "";
+        for (char character : expression.toCharArray()) {
+            if (isOperator(character)) {
+                tokens.add(constant(number));
+                tokens.add(operator(character));
+                number = "";
+                continue;
+            }
+            number += character;
+        }
+        tokens.add(constant(number));
+        return tokens;
+    }
+    
+    private boolean isOperator(char character) {
+        char[] operators = new char[]{'+', '-', '*', '/'};
+        for (char c : operators)
+            if (c == character) return true;
+        return false;
+    }
+    
+    private Operator operator(char character) {
+        if (character == '+') return Operator.ADD;
+        if (character == '-') return Operator.SUBTRACT;
+        if (character == '*') return Operator.MULTIPLY;
+        return Operator.DIVIDE;
+    }
+
+    private Token constant(String number) {
+        return new Constant(Integer.valueOf(number));
     }
     
 }
--- test/lexer/LexerTests.java
+++ test/lexer/LexerTests.java
@@ -1,5 +1,6 @@
 package lexer;
 
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import parser.shuntingyard.ShuntingYardParser;
@@ -11,8 +12,16 @@ public class LexerTests {
     @Test
     public void testAnalyzeSimpleExpression() {
         Lexer lexer = new Lexer();
-        Token[] tokens = lexer.analyze("1+2");
+        ArrayList<Token> tokens = lexer.analyze("1+2");
         ShuntingYardParser parser = new ShuntingYardParser(new SimpleParserTreeBuildingStrategy());
-        assertEquals(3, parser.parse(tokens).evaluate());
+        assertEquals(3, parser.parse(tokens.toArray(new Token[]{})).evaluate());
+    }
+    
+    @Test
+    public void testAnalyzeAnotherSimpleExpression() {
+        Lexer lexer = new Lexer();
+        ArrayList<Token> tokens = lexer.analyze("2-2");
+        ShuntingYardParser parser = new ShuntingYardParser(new SimpleParserTreeBuildingStrategy());
+        assertEquals(0, parser.parse(tokens.toArray(new Token[]{})).evaluate());
     }
 }
