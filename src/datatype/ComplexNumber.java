d1684df7cdb5718894fd51883dd17049a3166d0a
rubendm <rubendm@192.168.1.14>
2014-03-22 18:42:20 +0000

    ComplexNumber data type created

---
+++ src/datatype/ComplexNumber.java
@@ -0,0 +1,28 @@
+package datatype;
+
+public class ComplexNumber<TypeReal extends Number, TypeImaginary extends Number> {
+    
+    private final TypeReal real;
+    private final TypeImaginary imaginary;
+
+    public ComplexNumber(TypeReal real, TypeImaginary imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public TypeReal getReal() {
+        return real;
+    }
+
+    public TypeImaginary getImaginary() {
+        return imaginary;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof ComplexNumber)) return false;
+        ComplexNumber complexNumber = (ComplexNumber) obj;
+        return ((complexNumber.getReal() == real) && (complexNumber.getImaginary() == imaginary));
+    }
+}
