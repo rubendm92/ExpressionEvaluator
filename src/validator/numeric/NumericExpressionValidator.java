864500a34c748a25ff436117dcfae5d28ac71ed0
rubendm <rubendm@192.168.1.14>
2014-03-24 17:53:46 +0000

    Refactored

--- src/validator/numeric/NumericExpressionValidator.java
+++ src/validator/numeric/NumericExpressionValidator.java
@@ -9,13 +9,12 @@ public class NumericExpressionValidator implements ExpressionValidator {
     private ArrayList<Character> numbers;
     private ArrayList<Character> operators;
     
-    private boolean lastCharacterWasNumber = false;
+    private boolean lastCharacterWasNumber;
     private int parenthesis;
 
     public NumericExpressionValidator() {
-        this.parenthesis = 0;
-        initOperators();
         initNumbers();
+        initOperators();
     }
 
     private void initNumbers() {
@@ -34,35 +33,52 @@ public class NumericExpressionValidator implements ExpressionValidator {
     
     @Override
     public void check(String expression) {
+        init();
         char[] expressionWithoutSpaces = expression.replace(" ", "").toCharArray();
-        for (char character : expressionWithoutSpaces) {
-            if (isNumber(character) || character == '.') {
-                lastCharacterWasNumber = true;
-                continue;
-            }
-            if (isOperator(character) && lastCharacterWasNumber) {
-                lastCharacterWasNumber = false;
-                continue;
-            }
-            if (character == '(') {
-                parenthesis++;
-                continue;
-            }
-            if (character == ')' && lastCharacterWasNumber) {
-                parenthesis--;
-                continue;
-            }
-            throw  new InvalidExpressionException();
-        }
-        if (!(lastCharacterWasNumber && (parenthesis == 0))) throw  new InvalidExpressionException();
+        for (char character : expressionWithoutSpaces)
+            checkCharacter(character);
+        if (!lastCharacterIsValid()) throw  new InvalidExpressionException();
     }
 
+    private void init() {
+        lastCharacterWasNumber = false;
+        parenthesis = 0;
+    }
+    
+    private void checkCharacter(char character) throws InvalidExpressionException {
+        if (!isValidCharacter(character)) throw  new InvalidExpressionException();
+        process(character);
+    }
+    
+    private boolean isValidCharacter(char character) {
+        return (isNumber(character) || (characterAfterNumber(character)) || (character == '('));
+    
+    }
     private boolean isNumber(char character) {
         return numbers.contains(character);
+    }
+    
+    private boolean characterAfterNumber(char character) {
+        return (lastCharacterWasNumber && (character == '.' || isOperator(character) || character == ')'));
+    }
+    
+    private void process(char character) {
+        if (isNumber(character)) lastCharacterWasNumber = true;
+        else if (lastCharacterWasNumber) whenLastCharacterWasNumber(character);
+        else if (character == '(') parenthesis++;
+    }
+
+    private void whenLastCharacterWasNumber(char character) {
+        if (character == '.') lastCharacterWasNumber = false;
+        if (isOperator(character)) lastCharacterWasNumber = false;
+        if (character == ')') parenthesis--;
     }
     
     private boolean isOperator(char character) {
         return operators.contains(character);
     }
     
+    private boolean lastCharacterIsValid() {
+        return (lastCharacterWasNumber && (parenthesis == 0));
+    }
 }
